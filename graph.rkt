9dc6a8e06377f011621ab72e6445503442d331ae
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-09-05 17:24:22 -0300

    Implement connected-components#

--- graph.rkt
+++ graph.rkt
@@ -1,8 +1,13 @@
 #lang racket/base
 (require racket/match
          racket/port
+         racket/set
          tests/eli-tester
          (planet jaymccarthy/dijkstra))
+
+;------------------------------------------------------------
+; Definitions
+;------------------------------------------------------------
 
 ; a graph consists of:
 ; nodes#      = number
@@ -11,7 +16,8 @@
 
 ; Load an *undirected* graph structure from file.
 ; The first line is the number of nodes in the graph.
-; The next lines are pair of numbers representing one symmetric edge.
+; The next lines are pair of numbers representing one
+; symmetric edge.
 ;
 ; input-port -> graph
 (define (load-graph port)
@@ -28,36 +34,71 @@
            (hash-update! adjacencies node-to (λ (node-list) (cons node-from node-list)) '()))]))
     adjacencies))
 
-; input-port -> (listOf cons)
+; input-port -> (listOf pair)
 (define (load-edges port)
   (for/list ([node-from (in-producer read eof port)]
              [node-to (in-producer read eof port)])
     (cons node-from node-to)))
 
-; graph -> number
-(define (connected-components# graph)
-  0)
 
+;------------------------------------------------------------
+; Algorithms
+;------------------------------------------------------------
+
+(define (graph-shortest-path g src [stop? (lambda (node) #f)])
+  (shortest-path (lambda (node) (hash-ref (graph-adjacencies g) node '())) ; node-edges
+                 (lambda (edge) 1)                                         ; edge-weight
+                 (lambda (edge) edge)                                      ; edge-next
+                 src
+                 stop?))
+
+(define (connected-component g node)
+  (define-values (dist _)
+    (graph-shortest-path g node))
+  (list->set (hash-keys dist)))
+
+; graph -> number
+(define (connected-components# g)
+  (let-values ([(components _)
+   (for/fold ([components 0]
+              [visited (set)])
+             ([node (in-range 1 (add1 (graph-nodes# g)))]
+              #:when (not (set-member? visited node)))
+     (values
+      (add1 components)
+      (set-union visited (connected-component g node))))])
+    components))
+
+
+;------------------------------------------------------------
+; Computations
+;------------------------------------------------------------
 
 (define small-graph
   (call-with-input-file "small_graph.txt"
     (λ (in) (load-graph in))))
 
-(define (graph-shortest-path g src [stop? (lambda (node) #f)])
-  (shortest-path (lambda (node) (hash-ref (graph-adjacencies g) node '()))
-                 (lambda (edge) 1)
-                 (lambda (edge) edge)
-                 src
-                 stop?))
-
 (define-values (dist1 prev1)
-  (graph-shortest-path small-graph 1))
+  (graph-shortest-path small-graph 10))
 
 small-graph
 dist1
 prev1
-
 (shortest-path-to prev1 5)
+(connected-component small-graph 10)
+(connected-component small-graph 1)
+
+
+(define as-graph
+  (call-with-input-file "as_graph.txt"
+    (λ (in) (load-graph in))))
+
+(connected-components# as-graph)
+
+
+;------------------------------------------------------------
+; Tests
+;------------------------------------------------------------
 
 (define (all-tests)
   (let* ([nodes# 5]
@@ -80,6 +121,6 @@ prev1
      =>
      test-graph
      
-     (connected-components# test-graph) => 1)))
+     (connected-components# test-graph) => 3)))
 
 (all-tests)
